0fc78942f96c6392a989821f06e7c3d66bde8559
talkware <timtanbin@gmail.com>
2013-04-27 11:58:35 -0700

    test

--- TestJava/src/TestJava.java
+++ TestJava/src/TestJava.java
@@ -2,6 +2,6 @@ public class TestJava {
 
     public static void main(final String[] args) {
         final int a = (int) Long.MAX_VALUE;
-        System.out.println(a);
+        System.out.println(a + 1);
     }
 }
